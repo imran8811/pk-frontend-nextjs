7547bcf68f2a09b45f0309a8c4e5b393e2a6bda6
Mohd Imran <m.imran8811@gmail.com>
2024-06-19 16:05:34 +0500

    changes

--- app/wholesale-shop/checkout/page.tsx
+++ app/wholesale-shop/checkout/page.tsx
@@ -3,12 +3,12 @@ import Header from '../../../components/shared/header/header.comp';
 import Footer from '../../../components/shared/footer/footer.comp';
 import CheckoutComp from '../../../components/checkout/checkout.comp';
 import { useEffect } from 'react';
-import { CheckUserSession } from '../../../services/auth.service';
+import { getUserSessionData } from '../../../services/auth.service';
 import { redirect } from 'next/navigation';
 
 const CheckoutPage = () => {
   useEffect(() => {
-    if(!CheckUserSession()){
+    if(!getUserSessionData()){
       redirect('/login')
     }
   })
