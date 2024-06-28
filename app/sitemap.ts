5c7ee2ee53c10da8881d6a85552d658892e29471
Mohd Imran <m.imran8811@gmail.com>
2024-06-28 13:22:31 +0500

    changes

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+
+
+export default async function sitemap() {
+  const routes = [
+    "/",
+    "/wholesale-shop",
+    "/wholesale-shop/men/jeans-pant",
+    "/wholesale-shop/men/chino-pant",
+    "/wholesale-shop/boys/jeans-pant",
+    "/jeans-manufacturers",
+    "/bulk-jeans",
+    "/jeans-wholesale",
+  ];
+  routes.map((route) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+ 
+  return [...routes];
+}
