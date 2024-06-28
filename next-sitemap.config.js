57bd48ade8a339262fdc14949e2c5eb5dcf46729
Mohd Imran <m.imran8811@gmail.com>
2024-06-28 14:34:38 +0500

    changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,40 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://www.pkapparel.com',
-    generateRobotsTxt: false,
-    changefreq : "weekly",
-    exclude: ["/admin/*", "/login", "/signup", "/forgot-password", "/about", "/contact", "/factory", "/blog"]
+  siteUrl: process.env.SITE_URL || 'https://www.pkapparel.com',
+  generateRobotsTxt: false,
+  changefreq: "weekly",
+  priority: 1.0,
+  exclude: [
+    "/admin",
+    "/admin/*",
+    "/login",
+    "/signup",
+    "/forgot-password",
+    "/about",
+    "/contact",
+    "/factory",
+    "/blog",
+    "/order-confirmed",
+    "/file-upload",
+    "/orders",
+    "/robots.txt"
+  ],
+  additionalPaths: async (config) => {
+    const result = [];
+    result.push(
+      {
+        loc: '/wholesale-shop/men/jeans-pant',
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/wholesale-shop/men/chino-pant',
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/wholesale-shop/menboys/jeans-pant',
+        lastmod: new Date().toISOString(),
+      }
+    )
+    return result;
+  }
 }
