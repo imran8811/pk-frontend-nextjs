3ec43ac1a2593a3f08a0f06249d25e123b62980d
Mohd Imran <m.imran8811@gmail.com>
2024-06-28 14:15:16 +0500

    changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,6 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://www.pkapparel.com',
     generateRobotsTxt: false,
-    changefreq : "weekly"
+    changefreq : "weekly",
+    exclude: ["/admin/*", "/login", "/signup", "/forgot-password", "/about", "/contact", "/factory", "/blog"]
 }
