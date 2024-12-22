45823546775ced4f9e7dc567ca1bfb964ffc364b
Mohd Imran <m.imran8811@gmail.com>
2024-12-22 18:32:22 +0500

    changes

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,14 +2,15 @@
 import Header from '../../components/shared/header/header.comp';
 import Footer from '../../components/shared/footer/footer.comp';
 import CartComp from '../../components/cart/cart.comp';
+import { Suspense } from 'react';
 
 const CartPage = () => {
   return (
-    <>
+    <Suspense>
       <Header />
       <CartComp />
       <Footer />
-    </>
+    </Suspense>
   )
 }
 
