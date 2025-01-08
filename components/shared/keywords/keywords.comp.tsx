1c454af91049874c7c31a43d8767fc5e93220b24
Mohd Imran <m.imran8811@gmail.com>
2025-01-08 10:49:40 +0500

    changes

--- components/shared/keywords/keywords.comp.tsx
+++ components/shared/keywords/keywords.comp.tsx
@@ -5,28 +5,28 @@ const Keywords: FC = () => {
   return (
       <div className="row p-5 border-bottom border-info">
         <ul className="col-12 col-md-6 col-lg-3 mb-3">
-          <li><Link href="wholesale-shop/men/jeans-pants">Men Jeans Pants</Link></li>
-          <li><Link href="wholesale-shop/women/jeans-pants">Women Jeans Pants</Link></li>
-          <li><Link href="wholesale-shop/men/t-shirts">Men T-shirts</Link></li>
-          <li><Link href="wholesale-shop/girls/jeans-pants">Girls Jeans Pants</Link></li>
-          <li><Link href="wholesale-shop/men/chino-pants">Men Chino Pants</Link></li>
+          <li><Link href="/wholesale-shop/men/jeans-pants">Men Jeans Pants</Link></li>
+          <li><Link href="/wholesale-shop/women/jeans-pants">Women Jeans Pants</Link></li>
+          <li><Link href="/wholesale-shop/men/t-shirts">Men T-shirts</Link></li>
+          <li><Link href="/wholesale-shop/girls/jeans-pants">Girls Jeans Pants</Link></li>
+          <li><Link href="/wholesale-shop/men/chino-pants">Men Chino Pants</Link></li>
         </ul>
         <ul className="col-12 col-md-6 col-lg-3 mb-3">
-          <li><Link href="bulk-jeans">Bulk Jeans</Link></li>
-          <li><Link href="jeans-manufacturers">Jeans Manufacturers</Link></li>
-          <li><Link href="jeans-pants-manufacturers">Jeans Pants Manufacturers</Link></li>
-          <li><Link href="jeans-wholesale">Jeans Wholesale</Link></li>
+          <li><Link href="/bulk-jeans">Bulk Jeans</Link></li>
+          <li><Link href="/jeans-manufacturers">Jeans Manufacturers</Link></li>
+          <li><Link href="/jeans-pants-manufacturers">Jeans Pants Manufacturers</Link></li>
+          <li><Link href="/jeans-wholesale">Jeans Wholesale</Link></li>
         </ul>
         <ul className="col-12 col-md-6 col-lg-3 mb-3">
-          <li><Link href="jeans-manufacturing-cost">Jeans Manufacturing Cost</Link></li>
-          <li><Link href="wholesale-denim-jeans-suppliers">Wholesale Denim Jeans Suppliers</Link></li>
-          <li><Link href="wholesale-jeans-manufacturers">Wholesale Jeans Manufacturers</Link></li>
-          <li><Link href="wholesale-jeans-suppliers">Wholesale Jeans Suppliers</Link></li>
+          <li><Link href="/jeans-manufacturing-cost">Jeans Manufacturing Cost</Link></li>
+          <li><Link href="/wholesale-denim-jeans-suppliers">Wholesale Denim Jeans Suppliers</Link></li>
+          <li><Link href="/wholesale-jeans-manufacturers">Wholesale Jeans Manufacturers</Link></li>
+          <li><Link href="/wholesale-jeans-suppliers">Wholesale Jeans Suppliers</Link></li>
         </ul>
         <ul className="col-12 col-md-6 col-lg-3 mb-3">
-          <li><Link href="motorcycle-jeans-manufacturers">Motorcycle Jeans Manufacturers</Link></li>
-          <li><Link href="wholesale-women-jeans">Wholesale Women Jeans</Link></li>
-          <li><Link href="wholesale-jeans-bulk">Wholesale Jeans Bulk</Link></li>
+          <li><Link href="/motorcycle-jeans-manufacturers">Motorcycle Jeans Manufacturers</Link></li>
+          <li><Link href="/wholesale-women-jeans">Wholesale Women Jeans</Link></li>
+          <li><Link href="/wholesale-jeans-bulk">Wholesale Jeans Bulk</Link></li>
         </ul>
       </div>
   )
