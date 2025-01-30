3db3e6adf5ee0f6182e648372ea020032760458d
Mohd Imran <m.imran8811@gmail.com>
2025-01-30 11:56:21 +0500

    changes

--- src/components/shared/header/header.comp.tsx
+++ src/components/shared/header/header.comp.tsx
@@ -5,22 +5,20 @@ import HeaderTop from '../header-top/header-top.comp';
 
 export default function Header() {
   return (
-    <>
-      <header>
-        <HeaderTop />
-        <div className={cls(styles.headerMain, 'border-bottom row mt-4 pb-3')}>
-          <div className="col-md-4 mb-3">
-            <Link href="/">
-              <img src="/images/logo.jpg" alt="logo" width={227} height={46} title="PK Apparel Home" />
-            </Link>
-          </div>
-          <ul className={cls(styles.headerMenu, 'col-md-8')}>
-            <li><Link href={'/wholesale-shop/men'}>Men</Link></li>
-            <li><Link href={'/wholesale-shop/women'}>Women</Link></li>
-            <li><Link href={'/wholesale-shop/boys'}>Boys</Link></li>
-            <li><Link href={'/wholesale-shop/girls'}>Girls</Link></li>
-          </ul>
+    <header>
+      <HeaderTop />
+      <div className={cls(styles.headerMain, 'border-bottom row mt-4 pb-3')}>
+        <div className="col-md-4 mb-3">
+          <Link href="/">
+            <img src="/images/logo.jpg" alt="logo" width={227} height={46} title="PK Apparel Home" />
+          </Link>
         </div>
-      </header>
-    </>
+        <ul className={cls(styles.headerMenu, 'col-md-8')}>
+          <li><Link href={'/wholesale-shop/men'}>Men</Link></li>
+          <li><Link href={'/wholesale-shop/women'}>Women</Link></li>
+          <li><Link href={'/wholesale-shop/boys'}>Boys</Link></li>
+          <li><Link href={'/wholesale-shop/girls'}>Girls</Link></li>
+        </ul>
+      </div>
+    </header>
 )}
